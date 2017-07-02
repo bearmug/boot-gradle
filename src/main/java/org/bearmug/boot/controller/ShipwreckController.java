f1011cb9f7fd0f8e3266e3098e6478036eab1ee1
bearmug <pavel.fadeev@gmail.com>
2017-07-02 23:54:53 +0300

    #4 dummy list return

---
+++ src/main/java/org/bearmug/boot/controller/ShipwreckController.java
@@ -0,0 +1,18 @@
+package org.bearmug.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/")
+public class ShipwreckController {
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
+    public List<org.bearmug.boot.model.Shipwreck> list() {
+        return Collections.emptyList();
+    }
+}
