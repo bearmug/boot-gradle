67fa7b5eba6bbd415f4999d5e3f52bed8f052b1e
bearmug <pavel.fadeev@gmail.com>
2017-07-08 16:42:14 +0300

    #6 configure entity persistence

---
+++ src/main/java/org/bearmug/boot/reposotiry/ShipwreckRepository.java
@@ -0,0 +1,7 @@
+package org.bearmug.boot.reposotiry;
+
+import org.bearmug.boot.model.Shipwreck;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
+}
