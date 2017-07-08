54347924fbab627858797cc2c8664749c7242c36
bearmug <pavel.fadeev@gmail.com>
2017-07-08 16:48:25 +0300

    #6 plug in repo to controller

--- src/main/java/org/bearmug/boot/reposotiry/ShipwreckRepository.java
+++ src/main/java/org/bearmug/boot/reposotiry/ShipwreckRepository.java
@@ -4,4 +4,5 @@ import org.bearmug.boot.model.Shipwreck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
+
 }
